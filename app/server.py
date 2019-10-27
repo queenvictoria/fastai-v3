4a8bdfe605eedcefbdde51464749bf5c30de9c59
Mr Snow <mr.snow@houseoflaudanum.com>
2019-10-27 12:04:44 +1100

    Add whales trained model.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1XQVL7m5q_XRMRkBXb4L7gVnHG8_tF7mT'
+export_file_name = 'export-whales.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['blue', 'humpback', 'southernright']
 path = Path(__file__).parent
 
 app = Starlette()
